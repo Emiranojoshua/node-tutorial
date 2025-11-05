1bdfca9a5367f54fba4b34af4e3e3989a85ecea1
Emmanuel Joshua <emiranojoshua@gmail.com>
2025-11-05 17:44:42 +0100

    promises (old)

--- myfirst.js
+++ myfirst.js
@@ -1,17 +1,12 @@
-const myArray = [1, 2, { body: "some body", about: "some about" }];
-// let result = [];
-// myArray.forEach((arr, index) => {
-//   result.push(arr);
-// });
-
-// console.log(result);
-
-function ForEach(callback, array) {
-  for (let index = 0; index < array.length; index++) {
-    callback(index, array[index]);
-  }
+function myFunction() {
+  return new Promise((resolve, reject) => {
+    const val = false;
+    if (val) {
+      resolve("resolved successfully");
+    }
+    reject("rejected failed");
+  });
 }
 
-ForEach((index, indexValue) => {
-  console.log(indexValue)
-}, myArray);
+myFunction().then((value) => console.log(value)).catch(err => console.log(err));
+
