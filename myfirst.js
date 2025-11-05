99f13ec070cc960077e07cc048e9c4cd617b5d5d
Emmanuel Joshua <emiranojoshua@gmail.com>
2025-11-05 12:48:54 +0100

    custom foreach loop

--- myfirst.js
+++ myfirst.js
@@ -1,40 +1,17 @@
-// function myDisplayer(something) {
-//   document.getElementById("demo").innerHTML = something;
-// }
-
-// function myCalculator(num1, num2, myCallback) {
-//   let sum = num1 + num2;
-//   const value = myCallback(sum);
-//   return value;
-// }
-
-// const value = myCalculator(5, 5, (sum) => {
-//   return sum * 2;
+const myArray = [1, 2, { body: "some body", about: "some about" }];
+// let result = [];
+// myArray.forEach((arr, index) => {
+//   result.push(arr);
 // });
 
-// console.log(value);
-// console.log('Start');
-// setTimeout(myFunction, 1000);
-// setInterval(() => {
-//   console.log("interval set")
-// }, 1000);
+// console.log(result);
 
-// function myFunction() {
-//   console.log('lorem ipsum');
-// }
-// console.log('Hello World!');
-
-const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-
-function array_map(func, arr) {
-  let emptyArray = [];
-  for (let index = 0; index < arr.length; index++) {
-    emptyArray += [func(arr[index])];
+function ForEach(callback, array) {
+  for (let index = 0; index < array.length; index++) {
+    callback(index, array[index]);
   }
-
-  return emptyArray;
 }
 
-const mappedArray = array_map((item) => item + item, myArray);
-
-console.log(mappedArray);
+ForEach((index, indexValue) => {
+  console.log(indexValue)
+}, myArray);
