b9000f2fbf69df0ce8bc3e6215e08bd0d1563ba8
Emmanuel Joshua <emiranojoshua@gmail.com>
2025-11-05 17:49:12 +0100

    async await

--- myfirst.js
+++ myfirst.js
@@ -8,5 +8,18 @@ function myFunction() {
   });
 }
 
-myFunction().then((value) => console.log(value)).catch(err => console.log(err));
+myFunction()
+  .then((value) => console.log(value))
+  .catch((err) => console.log(err));
 
+async function init() {
+  try {
+    let res = await fetch("url");
+    let result = await res.json();
+    console.log(result);
+  } catch (error) {
+    console.log("something went wrong");
+  }
+}
+
+init();
